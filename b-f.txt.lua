9af5b30eea1d54902ff57b86e486349f10098d29
Joshamaruu <108309296+Joshamaruu@users.noreply.github.com>
2022-06-29 03:16:08 -0500

    Update b-f.txt.lua

--- b-f.txt.lua
+++ b-f.txt.lua
@@ -11,3 +11,13 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/ma
 
 
 --autofarm, auto sea 2 - 3, auto buy, fruit,legendary sword etc, auto roll fruit, auto store, and so many more --
+
+
+
+
+
+_G.Color = Color3.fromRGB(0, 0, 255)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/NEWBF.lua"))()
+
+
+-- Personally My favorite if you like custom stuff, you can change the rgb to any color you wish.
