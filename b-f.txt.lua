ec24ba566bde30f6355f7fae334ba791aa640ea0
Joshamaruu <108309296+Joshamaruu@users.noreply.github.com>
2022-06-28 23:21:11 -0500

    Create b-f.txt.lua

---
+++ b-f.txt.lua
@@ -0,0 +1,13 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/ATR",true))()
+
+
+
+
+
+
+
+
+
+
+
+--autofarm, auto sea 2 - 3, auto buy, fruit,legendary sword etc, auto roll fruit, auto store, and so many more --
