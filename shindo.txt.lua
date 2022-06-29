58ff37701b0032fba0d597db8a246eb372f9152d
Joshamaruu <108309296+Joshamaruu@users.noreply.github.com>
2022-06-29 06:25:21 -0500

    Update shindo.txt.lua

--- shindo.txt.lua
+++ shindo.txt.lua
@@ -1,1 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SxnwDev/Premier/main/PremierX-Free.lua", true))()
+
+
+
+_G.Color = Color3.fromRGB(0, 0, 255)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/NEWBF.lua"))()
